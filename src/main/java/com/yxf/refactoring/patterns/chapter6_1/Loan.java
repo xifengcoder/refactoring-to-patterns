b7f099654ef70f810121f5b8169750deec5d6a40
xifeng.yang <yxf05427@163.com>
2023-06-24 15:28:43 +0800

    refactoring 6.1: 使用Createtion Method替换Constructors

--- src/main/java/com/yxf/refactoring/patterns/chapter6_1/Loan.java
+++ src/main/java/com/yxf/refactoring/patterns/chapter6_1/Loan.java
@@ -10,21 +10,35 @@ public class Loan {
     private Date expiry;
     private CapitalStrategy capitalStrategy;
 
-    public Loan(double commitment, int riskRating, Date maturity) {
-        this(commitment, 0.00, riskRating, maturity, null);
+    public static Loan createTermLoan(double commitment, int riskRating, Date maturity) {
+        return new Loan(null, commitment, 0.00, riskRating, maturity, null);
     }
 
-    public Loan(double commitment, int riskRating, Date maturity, Date expiry) {
-        this(commitment, 0.00, riskRating, maturity, expiry);
+    public static Loan createTermLoan(CapitalStrategy capitalStrategy,
+                                      double commitment, double outstanding, int riskRating, Date maturity) {
+        return new Loan(capitalStrategy, commitment, outstanding, riskRating, maturity, null);
     }
 
-    public Loan(double commitment, double outstanding,
-                int riskRating, Date maturity, Date expiry) {
-        this(null, commitment, outstanding, riskRating, maturity, expiry);
+    public static Loan createResolver(double commitment, double outstanding, int riskRating, Date maturity, Date expiry) {
+        return new Loan(null, commitment, outstanding, riskRating, maturity, expiry);
     }
 
-    public Loan(CapitalStrategy capitalStrategy, double commitment,
-                int riskRating, Date maturity, Date expiry) {
+    public static Loan createRCTL(double commitment, double outstanding, int riskRating, Date maturity, Date expiry) {
+        return new Loan(null, commitment, outstanding, riskRating, maturity, expiry);
+    }
+
+    public static Loan createRCTL(CapitalStrategy capitalStrategy, double commitment, double outstanding, int riskRating, Date maturity, Date expiry) {
+
+        return new Loan(capitalStrategy, commitment, outstanding, riskRating, maturity, expiry);
+    }
+
+    public static Loan createResolver(CapitalStrategy capitalStrategy, double commitment,
+                                      int riskRating, Date maturity, Date expiry) {
+        return new Loan(capitalStrategy, commitment, riskRating, maturity, expiry);
+    }
+
+    private Loan(CapitalStrategy capitalStrategy, double commitment,
+                 int riskRating, Date maturity, Date expiry) {
         this(capitalStrategy, commitment, 0.00, riskRating, maturity, expiry);
     }
 
@@ -47,5 +61,17 @@ public class Loan {
                 this.capitalStrategy = new CapitalStrategyRCTL();
             }
         }
+    }
+
+    public int getRiskRating() {
+        return riskRating;
+    }
+
+    public Date getMaturity() {
+        return maturity;
+    }
+
+    public double getCommitment() {
+        return commitment;
     }
 }
--- src/test/java/com/yxf/refactoring/patterns/chapter6_1/CapitalCalculationTests.java
+++ src/test/java/com/yxf/refactoring/patterns/chapter6_1/CapitalCalculationTests.java
@@ -4,13 +4,17 @@ import org.junit.Test;
 
 import java.util.Date;
 
+import static org.junit.Assert.assertTrue;
+
 public class CapitalCalculationTests {
     @Test
     public void testTermLoanNoPayments() {
         double commitment = 1.2f;
         int riskRating = 9;
         Date maturity = new Date();
-        Loan termLoan = new Loan(commitment, riskRating, maturity);
+        Loan loan = Loan.createTermLoan(commitment, riskRating, maturity);
+        assert loan.getCommitment() == 1.2f;
+        assertTrue(loan.getRiskRating() == 9);
 
     }
 }
